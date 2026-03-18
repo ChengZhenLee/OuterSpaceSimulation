318bb9d3b31f7508364c4b6e6f291094e2b964c9
ChengZhenLee <zhensstories@gmail.com>
2026-03-18 18:44:28 +0100

    Fixed bugs and added test cases in main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include "constants.h"
 #include "visuals/visuals.h"
 
+// test
+#include "physics/physics.h"
+
 
 int main(void) {
-    InitWindow(windowWidth, windowHeight, title);
+    InitWindow(windowWidth, windowHeight, "Outer Space Simulation");
 
     // Create the camera object
     Camera3D camera = getCamera();
@@ -14,7 +17,25 @@ int main(void) {
 
     SetTargetFPS(60);
 
+    CelestialBody earth = {
+        "earth", WHITE, 1000.0f, 0.25f,
+        Eigen::Matrix<float, 3, 1>::Zero(),
+        Eigen::Matrix<float, 3, 1>::Zero(),
+        Eigen::Matrix<float, 3, 1>::Zero(),
+    };
+
+    CelestialBody earth2 = {
+        "earth2", WHITE, 10.0f, 0.25f,
+        Eigen::Matrix<float, 3, 1>(10, 0, 0),
+        Eigen::Matrix<float, 3, 1>(0, std::sqrt(G * 1000/10), 0),
+        Eigen::Matrix<float, 3, 1>::Zero(),
+    };
+
+    std::vector<CelestialBody> bodies = { earth, earth2 };
+
     while (!WindowShouldClose()) {
+        updateBodies(bodies);
+
         BeginDrawing();
 
         ClearBackground(BLACK);
@@ -23,10 +44,13 @@ int main(void) {
 
         BeginMode3D(camera);
             // Test draw a sphere
-            DrawSphere({0.0f, 1.0f, 1.0f}, 0.5f, WHITE);
+            for (CelestialBody body:bodies) {
+                Vector3 position = { body.position[0], body.position[1], body.position[2] };
+                DrawSphere(position, body.radius, WHITE);
+            }
 
             // Test draw a grid
-            DrawGrid(10, 1.0f); 
+            DrawGrid(1000, 1.0f); 
         EndMode3D();
 
         EndDrawing();
